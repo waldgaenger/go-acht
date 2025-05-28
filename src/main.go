d23bb8387aeb30745119be9ad9ed2b47793a3adb
waldgaenger <>
2025-05-28 04:43:11 +0200

    Simplified the main function. All the handling is now down by the Run()-function on the struct of the emulator

--- src/main.go
+++ src/main.go
@@ -3,41 +3,40 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
+	_ "net/http/pprof"
+	"os"
 
 	"github.com/waldgaenger/go-acht/src/chip8"
 )
+
+// const banner = `
+//  ▗▄▄▖ ▗▄▖      ▗▄▖  ▗▄▄▖▗▖ ▗▖▗▄▄▄▖
+// ▐▌   ▐▌ ▐▌    ▐▌ ▐▌▐▌   ▐▌ ▐▌  █
+// ▐▌▝▜▌▐▌ ▐▌    ▐▛▀▜▌▐▌   ▐▛▀▜▌  █
+// ▝▚▄▞▘▝▚▄▞▘    ▐▌ ▐▌▝▚▄▄▖▐▌ ▐▌  █
+// `
 
 var (
 	flagRom          = flag.String("rom", "", "Set this flag to provide a path to a ROM file.")
 	flagScale        = flag.Int("scale", 20, "Set this flag to provide a screen scale factor.")
 	flagColorProfile = flag.String("colorprofile", "black-white", "Set this flag to provide a color hprofile.")
+	// TODO: Implementing a debugging mode where we can constantly dump the registers to TTY
+	// TODO: Visualization of the internal state on the right side of the window
 )
 
 func main() {
 	flag.Parse()
 
-	if *flagRom != "" {
-		c8 := chip8.Chip8{}
-
-		c8.Init(int32(*flagScale), *flagColorProfile)
-		c8.LoadRom(*flagRom)
-
-		lastCycle := time.Now()
-		cylcleDelay := 16 * time.Millisecond
-
-		for c8.Running() {
-			currentTime := time.Now()
-			if time.Since(lastCycle) > time.Duration(cylcleDelay) {
-				lastCycle = currentTime
-				c8.Cycle()
-			}
-		}
-
-		c8.ShutDown()
-
-	} else {
+	if *flagRom == "" {
 		fmt.Println("You have to provide a ROM file")
 		flag.PrintDefaults()
+		return
+	}
+
+	c8 := chip8.Chip8{}
+
+	if err := c8.Run(*flagRom, int32(*flagScale), *flagColorProfile); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 }
